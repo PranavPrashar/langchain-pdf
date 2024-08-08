3dcf3e6ecc5a8de5f1c6101eed226f0af5543f5e
PranavPrashar <prasherlucky@gmail.com>
2024-08-07 20:44:01 -0400

    Working on adding the questions part

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import streamlit as st
 from PyPDF2 import PdfReader;
 
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings;
+from langchain.vectorstores import FAISS;
 
 def main():
     load_dotenv();
@@ -27,8 +29,14 @@ def main():
         text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200,length_function=len)
 
         chunks = text_splitter.split_text(text);
+        embeddings = OpenAIEmbeddings();
+        knowledge_base = FAISS.from_texts(chunks, embeddings);
 
-        st.write(chunks)
+        # st.write(chunks)
+
+        st.text_input("Enter a question about your PDF");
+
+
 
 
 if __name__ == '__main__':
